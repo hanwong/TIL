74d855b0646d51753ce8c19f56b00c8defd668d5
HanWoong Kim <hanationbear@gmail.com>
2016-11-08 22:59:23 +0900

    Initial Commit

--- nodejs/meadowlark/site/meadowlark.js
+++ nodejs/meadowlark/site/meadowlark.js
@@ -1,30 +1,40 @@
-var express = require('express');
+var express = require('express'),
+    app = express(),
+    // Handlebars 뷰 엔진 생성
+    handlebars = require('express-handlebars')
+    .create({defaultLayout:'main'});
 
-var app = express();
+app.use(express.static(__dirname + '/public'))
+    .set('port', process.env.PORT || 3000)
+    .engine('handlebars', handlebars.engine)
+    .set('view engine', 'handlebars');
 
-app.set('port', process.env.PORT || 3000);
+var fortunes = [
+    'Conquer your fears or they will conquer you.',
+    'Rivers need springs.',
+    'Do not fear what you don\'t know.',
+    'You will have a pleasant surprise.',
+    'Whenever possible, keep it simple.'
+];
 
 app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
-app.get('/about', function(res, req) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+app.get('/about', function(req, res) {
+    var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+    res.render('about', {fortune : randomFortune});
 });
 
 app.use(function(req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
