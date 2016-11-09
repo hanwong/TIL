ae564034bd2f8763dd58f506250657c59403065b
HanWoong Kim <hanationbear@gmail.com>
2016-11-09 23:11:46 +0900

    Add. partials and sections

--- nodejs/meadowlark/site/meadowlark.js
+++ nodejs/meadowlark/site/meadowlark.js
@@ -2,13 +2,55 @@ var express = require('express'),
     fortune = require('./lib/fortune'),
     app = express(),
     // Handlebars 뷰 엔진 생성
-    handlebars = require('express-handlebars')
-    .create({defaultLayout:'main'});
+    handlebars = require('express-handlebars').create({
+        defaultLayout:'main',
+        helpers: {
+            section: function(name, options) {
+                if(!this._sections) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
+    });
 
 app.use(express.static(__dirname + '/public'))
     .set('port', process.env.PORT || 3000)
     .engine('handlebars', handlebars.engine)
     .set('view engine', 'handlebars');
+
+function getWeatherData() {
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)'
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)'
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '54.5 F (12.5 C)'
+            }
+        ]
+    };
+}
+
+app.use(function(req, res, next) {
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData();
+    next();
+});
 
 app.get('/', function(req, res) {
     res.render('home');
