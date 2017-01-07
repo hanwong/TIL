be241aee9b4c17a6e37b968bab3c1a944104d020
HanWoong Kim <hanationbear@gmail.com>
2017-01-07 14:49:39 +0900

    vue study

---
+++ vue/README.md
@@ -0,0 +1,6 @@
+### VUEJS STUDY
+
+#### 컴포넌트 간 데이터 전달 방식
+
+- `props` PARENT => CHILD.
+- `$emit` CHILD => PARENT.
---
+++ vue/vue-cli/src/components/Server/ServerDetails.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+      <p v-if="!server">Please selected server.</p>
+      <p v-else>Server #{{ server.id }} selected, Status: {{ server.status }}</p>
+      <hr>
+      <button @click="resetStatus">ChangeNormal</button>
+    </div>
+
+</template>
+
+<script>
+  import { serverBus } from '../../main';
+  export default {
+    data: function() {
+      return {
+        server: null
+      }
+    },
+
+    methods: {
+      resetStatus() {
+        this.server.status = 'Normal';
+      }
+    },
+
+    created() {
+      serverBus.$on('serverSelected', (server) => {
+        this.server = server;
+      });
+    }
+  }
+</script>
+
+<style>
+
+</style>
